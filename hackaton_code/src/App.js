aef5e0cebc3ccbb11ddab3ec809838f0ca9e6b53
Pablo Boo <pablo.boo@udc.es>
2023-02-24 19:54:29 +0100

    Adición mapa picker

--- hackaton_code/src/App.js
+++ hackaton_code/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import MapPicker from 'react-google-map-picker'
+
+const DefaultLocation = { lat: 10, lng: 106};
+const DefaultZoom = 10;
+
+const App = () => {
+
+  const [defaultLocation, setDefaultLocation] = useState(DefaultLocation);
+
+  const [location, setLocation] = useState(defaultLocation);
+  const [zoom, setZoom] = useState(DefaultZoom);
+
+  function handleChangeLocation (lat, lng){
+    setLocation({lat:lat, lng:lng});
+  }
+  
+  function handleChangeZoom (newZoom){
+    setZoom(newZoom);
+  }
+
+  function handleResetLocation(){
+    setDefaultLocation({ ... DefaultLocation});
+    setZoom(DefaultZoom);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <button onClick={handleResetLocation}>Reset Location</button>
+  <label>Latitute:</label><input type='text' value={location.lat} disabled/>
+  <label>Longitute:</label><input type='text' value={location.lng} disabled/>
+  <label>Zoom:</label><input type='text' value={zoom} disabled/>
+  
+  <MapPicker defaultLocation={defaultLocation}
+    zoom={zoom}
+    mapTypeId="roadmap"
+    style={{height:'700px'}}
+    onChangeLocation={handleChangeLocation} 
+    onChangeZoom={handleChangeZoom}
+    apiKey='AIzaSyD07E1VvpsN_0FvsmKAj4nK9GnLq-9jtj8'/>
+  </>
   );
 }
 
-export default App;
+export default App
